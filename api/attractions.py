b7aaf46592f0270b4e3fc3438caa1d533af830e3
wei <kuo0930414695@gmail.com>
2022-03-06 13:28:27 +0800

    api完成及資料匯入資料庫中

--- api/attractions.py
+++ api/attractions.py
@@ -1,17 +1,36 @@
+
 from flask import Flask, render_template, request, session, redirect, url_for, Blueprint
 
+from utils.getattactions import get_attraction_by_page_keyword, get_attraction_by_id
+from utils.querystrcheck import query_str_check, query_by_id
+import json
 
+app_api_attractions = Blueprint('attraction', __name__, url_prefix='/api')
 
-app_api_attractions = Blueprint('attractions', __name__, url_prefix='/api')
+@app_api_attractions.route("/attractions", methods=['GET'])
+def attractions_by_page_keyword():
+    get_query_page = request.args.get('page')
+    get_query_keyword = request.args.get('keyword')
+    index_data_format, query_data_info = query_str_check(get_query_page,get_query_keyword)
+    if index_data_format:
+        query_data = get_attraction_by_page_keyword(query_data_info[0],query_data_info[1])
+        return  json.dumps(query_data)
+    else:
+        return  json.dumps(query_data_info)
 
-@app_api_attractions.route("/attractions", method="GET")
-def attractions():
-    pass
+@app_api_attractions.route("/attraction/<id>", methods=['GET'])
+def attraction_by_id(id):
+    index_data_format, query_id_info = query_by_id(id)
+    if index_data_format:
+        data = get_attraction_by_id(query_id_info)
+        return  json.dumps(data)
+    else:
+        return json.dumps(query_id_info)
 
-
-@app_api_attractions.route("/attraction/<id>", method="GET")
-def attractions(id):
-    pass
+@app_api_attractions.route("/attraction/", methods=['GET'])
+def attraction_no_insert_id():
+    data = {"error": True,"message": "請輸入 api/attraction/<id>"}
+    return json.dumps(data)
 
 
 
--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import *
+from api.attractions import app_api_attractions
+
 app=Flask(__name__)
 app.config["JSON_AS_ASCII"]=False
 app.config["TEMPLATES_AUTO_RELOAD"]=True
-
+app.register_blueprint(app_api_attractions)
 # Pages
 @app.route("/")
 def index():
@@ -17,4 +19,4 @@ def booking():
 def thankyou():
 	return render_template("thankyou.html")
 
-app.run(port=3000)
+app.run(port=3000, debug=True)
